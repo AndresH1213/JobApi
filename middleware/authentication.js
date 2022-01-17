027547b5ba30a6c6d192a1cd270cb9eff7de8c79
Andres Hernandez <edanhebla1213@hotmail.com>
2022-01-17 16:39:36 -0500

    authentication middleware set

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -13,7 +13,10 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name }
+    // this setup will be more useful in a complex project
+    const user = User.findById(payload.userId).select('-password')
+    req.user = user
+
     next()
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid')
